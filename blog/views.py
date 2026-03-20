fc48be0dfb0b0bdcef82e44c2a74cb7b1cbbf284
Carlos Jimenez H. <cjhirashi@gmail.com>
2026-03-20 11:53:23 -0600

    feat: Add blog index template and update view to render it with context data

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,29 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse('Página de blog')
+    """Blog home.
+
+    Por ahora solo renderiza el template mientras se definen los modelos.
+    """
+    
+    # Diccionario de datos para el Hero (Mantiene consistencia visual con la Home)
+    datos = {
+        'intro': 'Blog personal',
+        'titulo': 'Todos los artículos',
+        'descripcion': [
+            'Explora mis reflexiones, tutoriales y análisis en el mundo de la tecnología, la ciencia de datos y la ingeniería.',
+            'Sumérgete en un espacio de aprendizaje continuo, donde comparto mis experiencias y conocimientos para inspirar a otros profesionales y entusiastas del sector.',
+        ],
+    }
+    
+    # Construcción del contexto
+    context = {
+        'datos': datos,
+    }
+    
+    return render(
+        request, 
+        'index_blog.html',
+        context        
+    )
